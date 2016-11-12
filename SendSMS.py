01c2056e3745c8eeb02fcdb866afadea196a8868
ed01 <edoahmetspahic@gmail.com>
2016-11-11 18:35:01 -0800

    Update SendSMS.py

--- SendSMS.py
+++ SendSMS.py
@@ -8,8 +8,8 @@
 from twilio.rest import TwilioRestClient
 
 # put your own credentials here
-ACCOUNT_SID = "AC7eec02201b46e157430d1deb3df6c387"
-AUTH_TOKEN = "f4e5af03cec8d85df919db81b02fe522"
+ACCOUNT_SID = "XXXXXX"
+AUTH_TOKEN = "XXXXXX"
 
 client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN)
 
@@ -20,8 +20,8 @@ if (test == "something"):
     Message = input("sms message\n")
 
     client.messages.create(
-        to="9092731214",
-        from_="+19094927006",
+        to="XXXXX",
+        from_="XXXXXXX",
         body=Message,
     )
 
